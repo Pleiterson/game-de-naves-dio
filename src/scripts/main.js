561eab1c5a2ec4091f55140fec2ae61596ab3f05
pleiterson <pleiterson@gmail.com>
2021-01-12 16:18:38 -0300

    conclusao da aula 21

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -330,8 +330,24 @@ function start() {
 
         if (energiaAtual == 0) {
             $("#energia").css("background-image", "url(./src/assets/images/energia0.png)");
-            //Game Over
+            gameOver(); // chamando a função game over
         }
+    }
+
+    // função que avalia o fim de jogo, game over
+	function gameOver() {
+        fimdejogo = true;
+        musica.pause();
+        somGameover.play();
+        window.clearInterval(jogo.timer);
+        jogo.timer = null;
+        
+        $("#jogador").remove();
+        $("#inimigo1").remove();
+        $("#inimigo2").remove();
+        $("#amigo").remove();
+        $("#fundoGame").append("<div id='fim'></div>");
+        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
     }
 }
 
